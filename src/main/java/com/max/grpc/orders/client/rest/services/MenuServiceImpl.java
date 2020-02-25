c4f12f6eda740b356528a544de8249abe56f87fe
Maksym Hubach <mhuba@softserveinc.com>
2020-02-25 16:28:08 +0200

    Added CafeMenu mapper

--- src/main/java/com/max/grpc/orders/client/rest/services/MenuServiceImpl.java
+++ src/main/java/com/max/grpc/orders/client/rest/services/MenuServiceImpl.java
@@ -2,9 +2,9 @@
 package com.max.grpc.orders.client.rest.services;
 
 import com.max.grpc.orders.client.CafeClient;
+import com.max.grpc.orders.client.rest.mappers.CafeMenuMapperImpl;
 import com.max.grpc.orders.client.rest.mappers.FoodItemMapperImpl;
 import com.max.grpc.orders.client.rest.models.ApiCafeMenu;
-import com.max.grpc.orders.client.rest.models.ApiFoodItem;
 import com.max.grpc.orders.proto.CafeMenu;
 
 import org.apache.log4j.Logger;
@@ -13,18 +13,18 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Path("/menu")
 public class MenuServiceImpl {
     private CafeClient cafeClient;
     private FoodItemMapperImpl foodItemMapper;
+    private CafeMenuMapperImpl cafeMenuMapper;
     private final Logger logger = Logger.getLogger(MenuServiceImpl.class);
 
     public MenuServiceImpl(CafeClient cafeClient, FoodItemMapperImpl foodItemMapper) {
         this.cafeClient = cafeClient;
         this.foodItemMapper = foodItemMapper;
+        this.cafeMenuMapper = new CafeMenuMapperImpl();
     }
 
     @GET
@@ -35,13 +35,7 @@ public class MenuServiceImpl {
         CafeMenu cafeMenu = cafeClient.getMenu();
         logger.info("Received " + cafeMenu.getItemsCount() + " dishes");
 
-        ApiCafeMenu responseMenu = new ApiCafeMenu();
-        List<ApiFoodItem> resFoodItems = cafeMenu.getItemsList().stream()
-            .map(foodItemMapper::protoToRestModel)
-            .collect(Collectors.toList());
-        responseMenu.setItems(resFoodItems);
-
         logger.info("Response sent");
-        return responseMenu;
+        return cafeMenuMapper.protoToRestModel(cafeMenu);
     }
 }
